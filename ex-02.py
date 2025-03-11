1b763018afccc382f735bad665b70dfe744ac118
Bernardo-17 <bernardocandido6780@gmail.com>
2025-03-10 21:13:51 -0300

    Create ex-02.py

---
+++ ex-02.py
@@ -0,0 +1,4 @@
+#2.Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("Bernardo")
+print("Rio de Janeiro")
+print("Duque de Caxias")
