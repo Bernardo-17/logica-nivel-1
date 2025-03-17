5aca646b983a5f92d003133fa9d0edb0e976392c
Bernardo-17 <bernardocandido6780@gmail.com>
2025-03-17 19:56:00 -0300

    Create ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,6 @@
+#10 - Problema: escreva um software informe a média aritmética de um aluno que cursou 4 bimestres.
+bimestre1 = int(input("qual é a sua nota do primeiro bimestre?"))
+bimestre2 = int(input("qual é a sua nota do segundo bimestre?"))
+bimestre3 = int(input("qual é a sua nota do terceiro bimestre?"))
+bimestre4 = int(input("qual é a sua nota do terceiro bimestre?"))
+print((bimestre1 + bimestre2  + bimestre3  + bimestre4) / 4)
