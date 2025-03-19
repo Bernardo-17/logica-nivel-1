b4d20856d1177fe5688ba83e76922fd3ada4363b
Bernardo-17 <bernardocandido6780@gmail.com>
2025-03-19 20:09:51 -0300

    Create ex-16.py

---
+++ ex-16.py
@@ -0,0 +1,6 @@
+#16) As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos 12. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra.
+numero = int(input(" Digite o numero de maçâs que deseja?"))
+if(numero<12):
+    print (numero * 1.3)
+else:
+    print (numero * 1)
