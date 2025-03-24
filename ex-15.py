448a00003dadcf86db031185166a830c18962cb2
Bernardo-17 <bernardocandido6780@gmail.com>
2025-03-24 19:12:53 -0300

    Update ex-15.py

--- ex-15.py
+++ ex-15.py
@@ -1,5 +1,5 @@
 #15) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
-numero = int(input("Digite um numero!"))
+numero = int(input("Digite um numero:"))
 if(numero<0):
     print ("negativo")
 else:
