52f62a31ca02ab4e4e10e7c258e138399e53f2bc
Bernardo-17 <bernardocandido6780@gmail.com>
2025-03-24 21:56:16 -0300

    Update ex-17.py

--- ex-17.py
+++ ex-17.py
@@ -12,4 +12,18 @@ else:
         print(numero2)
     else:
         print(numero3)
+#  Ler 3 valores (considere que não serão informados valores iguais) e escrever o menor deles.
+numero1 = int(input("Coloque o primeiro numero:"))
+numero2 = int(input("Coloque o segundo numero:"))
+numero3 = int(input("Coloque o terceiro numero:"))
+
+
+if(numero1 < numero2):
+    if(numero1 < numero3):
+        print(numero1,"menor")
+else:
+    if(numero2<numero3):
+        print(numero2,"menor")
+    else:
+        print(numero3,"menor")
     
