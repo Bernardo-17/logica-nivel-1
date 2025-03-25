716b2ef96df8a6b67363b5c82577ee9dded0256f
Bernardo-17 <bernardocandido6780@gmail.com>
2025-03-25 19:04:21 -0300

    Update ex-18.py

--- ex-18.py
+++ ex-18.py
@@ -6,10 +6,13 @@ numero3 = int(input("Coloque o terceiro numero:"))
 if(numero1 < numero2):
     if(numero1 < numero3):
         print(numero2 + numero3)
+    else:
+        print(numero1 + numero2)
 else:
     if(numero2<numero3):
         print(numero3 + numero1)
     else:
         print(numero2 + numero1)
+
 
     
