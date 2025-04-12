da73700ea8a6856b82fc0e0406f17055c86c9104
Bernardo-17 <bernardocandido6780@gmail.com>
2025-04-11 21:37:15 -0300

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,17 @@
+#25) Somar números positivos digitados pelo usuário até que ele digite um número negativo
+
+nome_de_aluno = "bernardo"
+print("meu nome é " + nome_de_aluno +".")
+
+#Dois exemplos, porem o primeiro é a melhor forma.
+#4 tipos de dados primitivos de python:
+nome = "bernardo" #string
+idade = "17" # int
+altura = 1.72 
+gosto_de = "Programção"
+#com as váriaveis a cima posso mudar nas strings as informações a baixo
+print(f"Meu nome é {nome}.")
+print(f"Gosto de {gosto_de}.")
+print(f"Tenho {idade} anos. ")
+print(f"Meu é {nome}, gosto de {gosto_de}, tenho {idade} anos e com {altura} de altura.")
+
