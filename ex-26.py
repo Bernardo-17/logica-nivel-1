7e82be2630d55a74d588a9dabb2de7d0a290b817
Bernardo-17 <bernardocandido6780@gmail.com>
2025-05-19 18:56:49 -0300

    Create ex-26.py

---
+++ ex-26.py
@@ -0,0 +1,8 @@
+#26) Ler 5 números digitados pelo usuário e armazenar em um vetor. Depois, imprime os números informados.
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+n3 = int(input('Digite o terceiro numero: '))
+n4 = int(input('Digite o quarto numero: '))
+n5 = int(input('Digite o quinto numero: '))
+numero_usuario = [n1, n2, n3, n4, n5]
+print(numero_usuario)
