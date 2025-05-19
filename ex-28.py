f2e7b57765c95be6334d13512ae079ab2fc60ca9
Bernardo-17 <bernardocandido6780@gmail.com>
2025-05-19 19:41:42 -0300

    Update ex-28.py

--- ex-28.py
+++ ex-28.py
@@ -1,8 +1,8 @@
-#Crie um vetor com 5 números e imprima o último
+#Crie um vetor com 5 números e imprima o segundo
 n1 = int(input('Digite o primero numero: '))
 n2 = int(input('Digite o segundo numero: '))
 n3 = int(input('Digite o terceiro numero: '))
 n4 = int(input('Digite o quarto numero: '))
 n5 = int(input('Digite o quinto numero: '))
 numeros = [n1, n2, n3, n4, n5]
-print(f'Seu ultimo numero é {numeros[4]}')
+print(f'Seu segundo numero é {numeros[1]}')
