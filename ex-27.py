73750f527beb31c88cb0d408fb1517153185f4a9
Bernardo-17 <bernardocandido6780@gmail.com>
2025-05-19 19:24:07 -0300

    Update ex-27.py

--- ex-27.py
+++ ex-27.py
@@ -5,4 +5,4 @@ n3 = int(input('Digite terceiro numero: '))
 n4 = int(input('Digite quarto numero: '))
 n5 = int(input('Digite quinto numero: '))
 numeros = [n1, n2, n3, n4, n5]
-print(f'Seu primeiro número é {numeros[n1]}')
+print(f'Seu primeiro número é {numeros[0]}')
