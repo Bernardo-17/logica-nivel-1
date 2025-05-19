0659de806a847e3cfab1d1fcb4770b6b78c4d3d3
Bernardo-17 <bernardocandido6780@gmail.com>
2025-05-19 19:04:40 -0300

    Create ex-27.py

---
+++ ex-27.py
@@ -0,0 +1,8 @@
+#Crie um vetor com 5 números e imprima o primeiro valor
+n1 = int(input('Digite primeiro numero: '))
+n2 = int(input('Digite segundo numero: '))
+n3 = int(input('Digite terceiro numero: '))
+n4 = int(input('Digite quarto numero: '))
+n5 = int(input('Digite quinto numero: '))
+numeros = [n1, n2, n3, n4, n5]
+print(f'Seu primeiro número é {numeros[n1]}')
