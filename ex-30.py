45d74d2a3701ecaabebf449d306d27e1c683f058
Bernardo-17 <bernardocandido6780@gmail.com>
2025-05-19 21:09:49 -0300

    Create ex-30.py

---
+++ ex-30.py
@@ -0,0 +1,6 @@
+# Contar quantas vezes um número aparece em um vetor
+numeros = [5,5,5,2,3,5,6,]
+print(numeros)
+numero_usuario = int(input('Qual numero você quer contar: '))
+resposta = numeros.count(numero_usuario)
+print(resposta)
